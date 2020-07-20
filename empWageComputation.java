b9eec3bae2c2053cbd16e279ffa9b702e51e20bc
trampboy495 <histroylover@gmail.com>
2020-07-20 20:38:12 +0530

    [siddhesh] add.UC3 employee Part Time Wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 public class empWageComputation{
+	static int Wageper_Hr=20;
 	public static void checkAttendance(){
 		int isPresent=1;
 		int isAbsent=0;
@@ -7,17 +8,22 @@ public class empWageComputation{
 		int r=rand.nextInt(2);
 		if(r==isPresent){
 			System.out.println("Employee is present");
-			System.out.println("Employee daily wage is"+ getDailyWage());
+			System.out.println("Employee daily wage is "+ getDailyWage());
+			System.out.println("Employee Part Time wage is "+getPartTimeWage());
 		}
 		else{
 			System.out.println("Employee is absent");
 		}
 	}
 	public static int getDailyWage(){
-		int Wageper_Hr=20;
 		int Fullday_Hr=8;
 		int dailyWage=Wageper_Hr*Fullday_Hr;
 		return dailyWage;
+	}
+	public static int getPartTimeWage(){
+		int Partday_Hr=4;
+		int PartTimeWage=Wageper_Hr*Partday_Hr;
+		return PartTimeWage;
 	}
 	public static void main(String[]args){
 		System.out.println("welcome to employee wage computation");
