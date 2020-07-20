d5cdc50c4256995d321f3a40ee8d014d0198890c
trampboy495 <histroylover@gmail.com>
2020-07-20 20:20:08 +0530

    [siddhesh] add.UC2 employee daily wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,14 +1,23 @@
+import java.util.Random;
 public class empWageComputation{
 	public static void checkAttendance(){
 		int isPresent=1;
 		int isAbsent=0;
-		int r=(int)(Math.random()/2);
+		Random rand=new Random();
+		int r=rand.nextInt(2);
 		if(r==isPresent){
 			System.out.println("Employee is present");
+			System.out.println("Employee daily wage is"+ getDailyWage());
 		}
 		else{
 			System.out.println("Employee is absent");
 		}
+	}
+	public static int getDailyWage(){
+		int Wageper_Hr=20;
+		int Fullday_Hr=8;
+		int dailyWage=Wageper_Hr*Fullday_Hr;
+		return dailyWage;
 	}
 	public static void main(String[]args){
 		System.out.println("welcome to employee wage computation");
