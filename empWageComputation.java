1f631e10c56e0d017d84bca029b3ca73c42b7692
trampboy495 <histroylover@gmail.com>
2020-07-20 19:48:45 +0530

    [siddhesh] add.UC1 employee check attendance

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,5 +1,19 @@
 public class empWageComputation{
+	public static void checkAttendance(){
+		int isPresent=1;
+		int isAbsent=0;
+		int r=(int)(Math.random()/2);
+		if(r==isPresent){
+			System.out.println("Employee is present");
+		}
+		else{
+			System.out.println("Employee is absent");
+		}
+	}
 	public static void main(String[]args){
 		System.out.println("welcome to employee wage computation");
+		empWageComputation obj=new empWageComputation();
+		obj.checkAttendance();
 	}
+
 }
